301015cf20794abc4691ae3f4386f9b2af417da7
Nevo <nevillejemo@gmail.com>
2024-03-19 15:53:59 +0300

    adding the jwt header

--- collections1.py
+++ collections1.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from models import Collection, db
 from flask_restful import reqparse, Resource, Api
+from flask_jwt_extended import jwt_required
 
 
 collections_bp = Blueprint('collections_dp', __name__)
@@ -9,22 +10,27 @@ api = Api(collections_bp)
 
 class Collections(Resource):
     
+    @jwt_required()
     def get(self):
         collections = Collection.query.all()
         return collections
     
+    @jwt_required()
     def post(self):
         pass
     
 class CollectionsById(Resource):
+    
+    @jwt_required()
     def get(self, id):
         collection = Collection.query.filter_by(id=id).first()
         return collection
     
-    
+    @jwt_required()
     def patch(self, id):
         collection = Collection.query.filter_by(id=id).first()
         
+    @jwt_required()
     def delete(self, id):
         pass
     
