7471bc98f0829602f2172515bc7938a4baa2c122
Nevo <nevillejemo@gmail.com>
2024-03-20 06:53:52 +0300

    relevant comments for models.py

--- models.py
+++ models.py
@@ -1,22 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
+# Create SQLAlchemy instance
 db = SQLAlchemy()
 
+# Define User model
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
     
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String)
     password = db.Column(db.String)
     
     # Relationships
-    profile = db.relationship('Profile', back_populates='user', uselist=False)
-    collections = db.relationship('Collection', back_populates='user')
+    profile = db.relationship('Profile', back_populates='user', uselist=False)  # One-to-one relationship with Profile
+    collections = db.relationship('Collection', back_populates='user')  # One-to-many relationship with Collection
 
+# Define Profile model
 class Profile(db.Model, SerializerMixin):
     __tablename__ = 'profile'
     
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
@@ -25,15 +30,20 @@ class Profile(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
     # Relationships
-    user = db.relationship('User', back_populates='profile', uselist=False)
+    user = db.relationship('User', back_populates='profile', uselist=False)  # One-to-one relationship with User
 
+# Define Collection model
 class Collection(db.Model, SerializerMixin):
-    serialize_only = ('name','photo_url','user_id')
+    # Specify fields to be serialized
+    serialize_only = ('name', 'photo_url', 'user_id')
+    
     __tablename__ = 'collections'
+    
+    # Columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     photo_url = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))  
     
     # Relationships
-    user = db.relationship('User', back_populates='collections')
+    user = db.relationship('User', back_populates='collections')  # Many-to-one relationship with User
