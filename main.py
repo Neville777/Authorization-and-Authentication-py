70407db11a0616fa55bd8054439beeb895ec027a
Nevo <nevillejemo@gmail.com>
2024-03-20 06:42:34 +0300

    adding the comments in main,py

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db
 from datetime import timedelta
-from collections1 import collections_bp
-from auth import auth_bp, jwt
+from collections1 import collections_bp  
+from auth import auth_bp, jwt  
 
+# Create Flask application instance
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.db'
-app.config['SECRET_KEY']='jwt36v-12hrv2;sfqwf87a'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
-app.json.compact = False
-app.register_blueprint(collections_bp)
-app.register_blueprint(auth_bp)
+# Configure application settings
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.db'  # SQLite database URI
+app.config['SECRET_KEY'] = 'jwt36v-12hrv2;sfqwf87a'  # Secret key for JWT
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy modification tracking
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)  # JWT access token expiration time
+app.json.compact = False  # Compact JSON response format
 
-# Initialize Migrate
+# Register blueprints for different parts of the application
+app.register_blueprint(collections_bp)  
+app.register_blueprint(auth_bp)  
+
+# Initialize Flask-Migrate for database migrations
 db.init_app(app)
-jwt.init_app(app)
-
 migrate = Migrate(app, db)
 
+# Initialize JWT manager
+jwt.init_app(app)
+
+# run the Flask application
+
+if __name__ == "__main__":
+    app.run()
